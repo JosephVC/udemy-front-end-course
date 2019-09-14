aecaa1d59b316691e08860c510434e1a8f786f25
JosephVC <josephvcardenas@gmail.com>
2019-09-14 08:35:26 -0700

    module 210: redux async actions

--- src/actions.js
+++ src/actions.js
@@ -1,9 +1,20 @@
-import { CHANGE_SEARCH_FIELD } from './constants.js'
+import { 
+  CHANGE_SEARCH_FIELD,
+  RQUEST_ROBOTS_PENDING,
+  REQUEST_ROBOTS_SUCCESS,
+  REQUEST_ROBOTS_FAILED,
+  REQUEST_ROBOTS_PENDING
+} from './constants.js'
 
-export const setSearchField = (text) => {
-  console.log(text);
-  return {
+export const setSearchField = (text) => ({
     type: CHANGE_SEARCH_FIELD,
     payload: text
-  }
+  })
+
+export const requestRobots = (dispatch) => {
+  dispatch({ type: REQUEST_ROBOTS_PENDING });
+  fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(data => dispatch({ type:REQUEST_ROBOTS_SUCCESS, payload: data}))
+    .catch(error => dispatch( { type: REQUEST_ROBOTS_FAILED, payload: error}))
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import { searchRobots } from './reducers';
@@ -10,7 +11,8 @@ import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
 const logger = createLogger();
-const store = createStore(searchRobots, applyMiddleware(logger))
+const store = 
+  createStore(searchRobots, applyMiddleware(thunkMiddleware, logger))
 
 ReactDOM.render(
     <Provider store={store} >
