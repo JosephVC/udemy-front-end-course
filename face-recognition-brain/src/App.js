5bc3d220dc5cefa649e05b397137c1adbbe440a9
JosephVC <josephvcardenas@gmail.com>
2019-10-07 15:18:39 -0700

    created Logo folder and Logo.js

--- face-recognition-brain/src/App.js
+++ face-recognition-brain/src/App.js
@@ -1,14 +1,16 @@
 import React, {Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      {/* { <Logo />
-      <ImageLinkForm />
-      <FaceRecognition />} */}
+      <Logo /> 
+      {/* <ImageLinkForm />
+      <FaceRecognition />} */} 
     </div>
   );
 }
