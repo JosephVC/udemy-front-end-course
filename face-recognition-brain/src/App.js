0236fa50661561d435cffa17e93158651db9e0cc
JosephVC <josephvcardenas@gmail.com>
2020-03-11 06:57:39 -0700

    restarting tutorial!

--- face-recognition-brain/src/App.js
+++ face-recognition-brain/src/App.js
@@ -7,50 +7,17 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
 
-<<<<<<< HEAD
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo /> 
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
-    </div>
-  );
-=======
-const particlesOptions = {
-  particles: {
-    number: {
-      value:30,
-      density: {
-        enable: true,
-        value_area: 800
-      }
-    }
-    // line_linked: {
-    //   shadow: {
-    //     enable: true,
-    //     color: "#3CA9D1",
-    //     blur: 5
-      }
-}
-
-class App extends Component {
+class App extends Component{
   render() {
-     return (
-        <div className="App">
-          <Particles className ='particles' 
-                params={particlesOptions} 
-                />
-          <Navigation />
-          <Logo /> 
-          <Rank /> 
-          <ImageLinkForm />
-          {/* <FaceRecognition />} */}  
-        </div>
-      );
-    }
->>>>>>> 309555fcb76c978a4668a11f7bb24360de56f584
+    return (
+      <div className="App">
+        <Navigation />
+        <Logo /> 
+          {/* {<ImageLinkForm />
+         <FaceRecognition /> */} 
+      </div>
+    )
+  }
 }
 
 export default App;
